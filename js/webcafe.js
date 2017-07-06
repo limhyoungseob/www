ade18cf2c57020c41a02cbf92703f8bc9f5a24fc
limhyoungseob <do1it@naver.com>
2017-07-06 16:19:08 +0900

    중간 공지사항 자료실까지 완료

--- js/webcafe.js
+++ js/webcafe.js
@@ -1,4 +1,4 @@
-// 서브 메뉴 제어를 위한 스크립트
+// 메인 메뉴 제어를 위한 스크립트
 // jquery와 같은 기능한다. $// 여기서 this는 li 다.
 $(function() {
     $(".main-menu>li").hover(function() {
@@ -43,4 +43,17 @@ $(function() {
     //         $(this).parent("li").css("display", "none");
     //     }
     // );
+
+    // 탭메뉴 제어를 위한 자바스크립트(공지사항 자료실 부분)
+    // $(); 래퍼 함수 또는 팩토리 함수라고 부른다.
+    // click 보다는 focus를 사용하면 마우스와 키보드 두개를 동시에 사용할 수 있다.
+    $(".board h2").focus(function() {
+        $(this).parent().addClass("act").siblings().removeClass("act");
+    });
 });
+
+// $(function() {
+//     $(".board h2").click(function() {
+//         $(this).parent().addClass("act").siblings().removeClass("act");
+//     });
+// });
