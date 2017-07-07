d3fe22cfd42938a76a85468a9af8ba18cfdf41f8
limhyoungseob <do1it@naver.com>
2017-07-07 16:23:57 +0900

    인기사이트 디자인 완성

--- js/webcafe.js
+++ js/webcafe.js
@@ -50,6 +50,16 @@ $(function() {
     $(".board h2").focus(function() {
         $(this).parent().addClass("act").siblings().removeClass("act");
     });
+
+    // 관련사이트 키보드 포커스 시 클래스 추가 및 제거
+    $(".related-list a").focusin(function() {
+        $(this).parents(".related-list").addClass("list-act");
+
+    });
+    $(".related-list a").focusout(function() {
+        $(this).parents(".related-list").removeClass("list-act");
+
+    });
 });
 
 // $(function() {
